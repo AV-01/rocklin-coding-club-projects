710550bfc73a5e05c54e2916d2ee1d48c3d58bd7
Aryavrat Mishra <105894575+AV-01@users.noreply.github.com>
2024-10-02 10:40:31 -0700

    affine cipher

---
+++ affine_cipher.py
@@ -0,0 +1,20 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+plaintext = "hello"
+
+def encrypt(x, a, c):
+    return (a*x +c)%(len(alphabet))
+
+def encrypt_text(text):
+    array = []
+    for chr in text:
+        index = encrypt(alphabet.index(chr), 5, 2)
+        newchar = alphabet[index]
+        array.append(newchar)
+    return "".join(array)
+
+print(encrypt_text(plaintext))
+
+def decrypt(x, a, c):
+    for i in range(1, len(alphabet)):
+        if (i * a) % len(alphabet) == 1: return i
+    return -1
